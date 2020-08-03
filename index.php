6cd9d2af2d87cb562d2ec7df5608d4b89ee6c841
luciandex <remindfwd@gmail.com>
2020-08-03 18:16:02 +0300

    Index.php added with a small exercise.

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+// Sa se afiseze toate numerele din intervalul [1000,99999]
+// care au proprietatea ca atat ele cat si rasturnatul lor sunt prime.
+
+$n;
+$c;
+$d;
+$k;
+function prim($n): int
+{
+    if ($n == 0 || $n == 1) return 0;
+    for ($i = 2; $i <= $n * $n; $i++) ;
+    if ($n % $i == 0) return 0;
+    return 1;
+}
+
+// echo prim(6);
+
+function rast($n)
+{
+    $d = 0;
+    while ($n != 0) {
+        $c = $n % 10;
+        $d = $d * 10 + $c;
+        $n = $n / 10;
+    }
+    return $d;
+}
+
+function result()
+{
+    for ($i = 1001; $i <= 99999; $i++) ;
+    if (prim($i) && prim(rast($i)) result() << $i << end());
+}
+
+echo result();
